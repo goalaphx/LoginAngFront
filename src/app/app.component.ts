4c94aac10ff7b1654f91cf81da3eb10b6ec25599
El Mahdi Id Lahcen <idlahcen.elmahdi@etu.uae.ac.ma>
2024-10-30 22:39:56 +0100

    load project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
+import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+import { ActivatedRoute, RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,FormsModule,RouterLink,RouterLinkActive,CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
